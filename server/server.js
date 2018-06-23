34a576828b6e91dd1fe40665d2f15906621e390f
Jacob Albright <jacob.albright23@gmail.com>
2018-06-23 14:19:36 -0500

    wrote scraper results to .json file in root now that the scraper is working. Now, can work out cleaning and database insertion without scraping a ton

--- server/server.js
+++ server/server.js
@@ -6,11 +6,6 @@ const app = express();
 // Include NPR Scraper 
 const nprScraper = require('./modules/npr');
 
-
-
-
-
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -20,10 +15,16 @@ app.listen(process.env.PORT || 5000, () => {
   console.log('Server ready on port:', process.env.PORT || 5000);  
 });
 
+const fs = require('fs');
 
 app.get('/get-npr', async (req, res) => {
   const results = await nprScraper();  
-  res.send(results);
+  fs.writeFile('dataToClean.json', JSON.stringify(results, null, 4), (err) => {
+    if (err) throw err;
+    console.log('Check the roooooot');
+    
+  });
+  // res.send(results);
 })
 
 
