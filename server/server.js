b21ad6bee6c02f4a50bdd8061c00d84eb43eabbb
Jacob Albright <jacob.albright23@gmail.com>
2018-06-26 17:03:01 -0500

    removed the 'public' directory from server.js because I will not serve any client-side information, only json objects

--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 /** Routes */
 app.use('/api/article/data', nprDataRouter);
 
-app.use(express.static('server/public'))
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Server ready on port:', process.env.PORT || 5000);  
